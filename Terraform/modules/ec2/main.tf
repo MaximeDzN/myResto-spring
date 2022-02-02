4595a09a09989d918b7e21b7eff639c57fd46093
asemin08 <semin.allan@gmail.com>
2022-02-02 20:09:47 +0100

    Refactot popur ancien main.tf

--- Terraform/modules/ec2/main.tf
+++ Terraform/modules/ec2/main.tf
@@ -17,61 +17,22 @@ resource "aws_instance" "myresto-ec2" {
   tags = {
     Name = "${var.ec2_name}-ec2"
   }
-
-  provisioner "local-exec" {
-    command = "rm -f host && echo \"[master]\" > hosts && echo \"${format("%s ansible_ssh_user=%s", var.ip_public, var.utilisateur_ssh)}\" >> hosts"
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install software-properties-common",
+      "sudo add-apt-repository --yes --update ppa:ansible/ansible",
+      "sudo apt install --yes ansible",
+    ] 
+    connection {
+      type        = "ssh"
+      user        = "${var.utilisateur_ssh}"
+      private_key = file("../../.aws/${var.cle_ssh}.pem")
+      host        = "${self.public_ip}"
+    }
   }
 
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "sudo apt update -y",
-  #     "sudo apt install software-properties-common",
-  #     "sudo add-apt-repository --yes --update ppa:ansible/ansible",
-  #     "sudo apt install --yes ansible",
-  #     "git clone -b DevOps https://github.com/${var.git_proprietaire}/${var.git_projet}.git",
-  #     "cd ${var.git_projet}/ansible/",
-  #     "ansible-playbook -i hosts.yml myresto.yml"
-  #   ] 
-  #   connection {
-  #     type        = "ssh"
-  #     user        = "${var.utilisateur_ssh}"
-  #     private_key = file("../../.aws/${var.cle_ssh}.pem")
-  #     host        = "${self.public_ip}"
-  #   }
-  # }
 
-
-}
-
-
-resource "aws_instance" "myresto-ec2-worker1" {
-  ami               = data.aws_ami.ubuntu_bionic_ami.id
-  instance_type     = var.type_instance
-  key_name          = var.cle_ssh
-  availability_zone = var.zone_dispo
-  security_groups   = ["${var.securite_groupe}"]
-  tags = {
-    Name = "${var.ec2_name}-ec2-worker1"
-  }
-
-  provisioner "local-exec" {
-    command = "echo \"[workers]\" > hosts && echo \"${join("\n",formatlist("%s ansible_ssh_user=%s", var.ip_public, var.utilisateur_ssh))}\" >> hosts"
-  }
-}
-
-
-resource "aws_instance" "myresto-ec2-worker2" {
-  ami               = data.aws_ami.ubuntu_bionic_ami.id
-  instance_type     = var.type_instance
-  key_name          = var.cle_ssh
-  availability_zone = var.zone_dispo
-  security_groups   = ["${var.securite_groupe}"]
-  tags = {
-    Name = "${var.ec2_name}-ec2-worker2"
-  }
-
-  provisioner "local-exec" {
-    command = "echo \"${join("\n",formatlist("%s ansible_ssh_user=%s", var.ip_public, var.utilisateur_ssh))}\" >> hosts"
-  }
 }
 
