5874798ed5e0f285afda0783b651766861bce7bf
asemin08 <semin.allan@gmail.com>
2022-02-02 18:55:41 +0100

    Test Ajout EC2

--- Terraform/modules/ec2/main.tf
+++ Terraform/modules/ec2/main.tf
@@ -43,3 +43,75 @@ resource "aws_instance" "myresto-ec2" {
 
 }
 
+
+resource "aws_instance" "myresto-ec2-worker1" {
+  ami               = data.aws_ami.ubuntu_bionic_ami.id
+  instance_type     = var.type_instance
+  key_name          = var.cle_ssh
+  availability_zone = var.zone_dispo
+  security_groups   = ["${var.securite_groupe}"]
+  tags = {
+    Name = "${var.ec2_name}-ec2-worker1"
+  }
+
+  # provisioner "local-exec" {
+  #   command = "echo MyResto est disponible sur cette url : http://${var.ip_public}:8080/ >> ip_connection.txt"
+  # }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install software-properties-common",
+      "sudo add-apt-repository --yes --update ppa:ansible/ansible",
+      "sudo apt install --yes ansible",
+      "git clone -b DevOps https://github.com/${var.git_proprietaire}/${var.git_projet}.git",
+      "cd ${var.git_projet}/ansible/",
+      "ansible-playbook -i hosts.yml myresto.yml"
+    ] 
+    connection {
+      type        = "ssh"
+      user        = "${var.utilisateur_ssh}"
+      private_key = file("../../.aws/${var.cle_ssh}.pem")
+      host        = "${self.public_ip}"
+    }
+  }
+
+
+}
+
+
+resource "aws_instance" "myresto-ec2-worker2" {
+  ami               = data.aws_ami.ubuntu_bionic_ami.id
+  instance_type     = var.type_instance
+  key_name          = var.cle_ssh
+  availability_zone = var.zone_dispo
+  security_groups   = ["${var.securite_groupe}"]
+  tags = {
+    Name = "${var.ec2_name}-ec2-worker2"
+  }
+
+  # provisioner "local-exec" {
+  #   command = "echo MyResto est disponible sur cette url : http://${var.ip_public}:8080/ >> ip_connection.txt"
+  # }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install software-properties-common",
+      "sudo add-apt-repository --yes --update ppa:ansible/ansible",
+      "sudo apt install --yes ansible",
+      "git clone -b DevOps https://github.com/${var.git_proprietaire}/${var.git_projet}.git",
+      "cd ${var.git_projet}/ansible/",
+      "ansible-playbook -i hosts.yml myresto.yml"
+    ] 
+    connection {
+      type        = "ssh"
+      user        = "${var.utilisateur_ssh}"
+      private_key = file("../../.aws/${var.cle_ssh}.pem")
+      host        = "${self.public_ip}"
+    }
+  }
+
+
+}
+
