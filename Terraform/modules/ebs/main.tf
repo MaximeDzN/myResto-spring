0dd680dbde1e91f51f12bdff32182b13ac2dda77
asemin08 <semin.allan@gmail.com>
2022-02-02 20:42:10 +0100

    Ajout ebs

---
+++ Terraform/modules/ebs/main.tf
@@ -0,0 +1,9 @@
+
+resource "aws_ebs_volume" "volume" {
+  availability_zone = "${var.zone_dispo}"
+  size              = var.capacite_disque
+
+  tags = {
+    Name = "${var.auteur}-ebs"
+  }
+}
---
+++ Terraform/modules/ebs/variables.tf
@@ -0,0 +1,14 @@
+variable "auteur" {
+  type    = string
+  default = "projet2"
+}
+
+variable "capacite_disque" {
+  type    = number
+  default = 10
+}
+
+variable "zone_dispo" {
+  type    = string
+  default = "us-east-1b"
+}
