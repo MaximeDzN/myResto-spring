f76627af8217be41471e4ae7f5e4cc63c74e63e6
asemin08 <semin.allan@gmail.com>
2022-02-03 14:53:28 +0100

    test

---
+++ Docker/shell.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker-compose -f /home/ec2-user/myResto-spring/Docker/docker-compose.yaml up -d docker-mysql
+
+docker-compose -f /home/ec2-user/myResto-spring/Docker/docker-compose.yaml up -d phpmyadmin
