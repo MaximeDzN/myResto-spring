d81e594664eeec5ab74bf03cd046a8a372ab5260
DAZIN Maxime <maxime.dazin@gmail.com>
2022-02-03 23:08:25 +0100

    update security to able only owner to create items

--- src/main/java/eu/ensup/my_resto/config/SecurityConfig.java
+++ src/main/java/eu/ensup/my_resto/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package eu.ensup.my_resto.config;
 import eu.ensup.my_resto.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,8 +26,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/users","/items/add").hasAuthority("OWNER")
                 .antMatchers("/", "/register", "/js/**", "/styles/**","/images/**", "/img/**", "/assets/**","/items/**").permitAll()
-                .antMatchers("/users").hasRole("OWNER")
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
