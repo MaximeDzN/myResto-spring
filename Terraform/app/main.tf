bdfb5b7b5dfc9e2473a3f742d0070d7fdf70c163
asemin08 <semin.allan@gmail.com>
2022-02-03 17:28:49 +0100

    test

--- Terraform/app/main.tf
+++ Terraform/app/main.tf
@@ -38,6 +38,13 @@ resource "aws_volume_attachment" "ebs_to_ec2" {
       "sudo amazon-linux-extras install -y ansible2",
       "git clone -b DevOps https://github.com/${var.git_proprietaire}/${var.git_projet}.git",
       "cd ${var.git_projet}/ansible/", 
+      "sudo amazon-linux-extras install -y  docker",
+      "sudo yum install -y docker",
+      "sudo service docker start",
+      "sudo systemctl enable docker",
+      "sudo usermod -a -G docker ec2-user",
+      "sudo curl -L \"https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
+      "sudo chmod +x /usr/local/bin/docker-compose", 
       "ansible-playbook -i hosts.yml myresto.yml"
     ] 
     connection {
