e27edac59434abc1b4965266dc0dd357d71ddb58
asemin08 <semin.allan@gmail.com>
2022-02-04 02:17:16 +0100

    refactot ip statique

--- Terraform/app/main.tf
+++ Terraform/app/main.tf
@@ -4,13 +4,11 @@ module "sg" {
   auteur = "${var.auteur}"
 }
 
-# # Instanciation module eip
-# module "eip" {
-#   source        = "../modules/eip"
-#   auteur = "${var.auteur}"
-# }
-
-
+# Instanciation module eip
+module "eip" {
+  source        = "../modules/eip"
+  auteur = "${var.auteur}"
+}
 # Instanciation module ec2
 module "ec2" {
   source        = "../modules/ec2"
@@ -24,7 +22,7 @@ module "ec2" {
 
 resource "aws_eip_association" "eip_assoc" {
   instance_id = module.ec2.out_ec2_id
-  allocation_id = var.eip_id
+  allocation_id = module.eip.out_eip_id
 }
 
 
