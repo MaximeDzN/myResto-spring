2aab1df98208e33dfcca956e455199f27c124739
asemin08 <60793285+asemin08@users.noreply.github.com>
2022-02-04 04:40:00 +0100

    Delete shell.sh

--- Docker/shell.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker-compose -f /home/ec2-user/myResto-spring/Docker/docker-compose.yaml up -d docker-mysql
-
-# docker-compose -f /home/ec2-user/myResto-spring/Docker/docker-compose.yaml up -d
